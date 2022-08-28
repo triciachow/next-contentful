19ce75c6eb79236da9183166833c7c646f43f754
triciachow0212 <suyeechow0212@gmail.com>
2022-08-28 17:48:45 +0800

    Render basic recipe list and styling

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
-export default function Recipes() {
-  return <div className="recipe-list">Recipe List</div>;
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+import style from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  return (
+    <div className={style.recipe}>
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+    </div>
+  );
 }

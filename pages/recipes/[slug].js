541f1fe36581926d74a0ace810c1e3b60ff01f38
triciachow0212 <suyeechow0212@gmail.com>
2022-08-28 22:24:17 +0800

    Generate styling for each slug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,3 +1,65 @@
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+import { createClient } from "contentful";
+import Image from "next/image";
+import style from "../../styles/Instructions.module.css";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields;
+
+  return (
+    <div>
+      <div className={style.banner}>
+        <Image
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2 className={style.title}>{title}</h2>
+      </div>
+      <div className={style.info}>
+        <p>Take about {cookingTime} mins to cook.</p>
+        <h3 className={style.title}>Ingredients:</h3>
+        {ingredients.map(ingredient => (
+          <span key={ingredient}>{ingredient}</span>
+        ))}
+      </div>
+      <div className="">
+        <h3 className={style.title}>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+    </div>
+  );
 }
